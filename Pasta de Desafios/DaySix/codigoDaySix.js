ee998d8b35767318ff4c01ce4dfd8715bb37cc11
Allisson0 <allissonhiaggo@gmail.com>
2025-10-07 19:02:18 -0300

    Adição de CSS e animações e refatoração da escolha da loja

--- Pasta de Desafios/DaySix/codigoDaySix.js
+++ Pasta de Desafios/DaySix/codigoDaySix.js
@@ -5,6 +5,8 @@ const escCongelados = document.getElementById("congelados");
 const escDoces = document.getElementById("doces");
 const escOutros = document.getElementById("outros");
 
+const listaEscolhasHTML = [escFruta, escLaticineos, escCongelados, escDoces, escOutros];
+
 //Lista da loja e do carrinho
 const listaHTML = document.getElementById("listaLoja");
 const listaCarrinhoHTML = document.getElementById("listaCarrinho");
@@ -65,73 +67,30 @@ let listaOutros=[
     ['TV', 5000]
 ];
 
+let listaEscolhas = [listaFrutas, listaLaticineos, listaCongelados, listaDoces, listaOutros];
 
+//Loja
 //Funcoes de escolha das frutas atraves do clique do botao
 //Fruta = 1, Lat. = 2, Congelados = 3, Doces = 4, Outros = 5, neutro = 0
-//Se a variavel esc for diferente do indice do tipo, preenche a loja com o array do tipo
+//botaoPressionado se referencia ao objeto e .value busca o valor para ele, definido acima
+//Se a variavel esc for diferente do botPress, preenche a loja com o array do tipo
 //Se nao, reseta a lista da loja, reseta a listaAtiva e esc = 0.
-//Frutas
-function escolheFrutas(){
-    if (esc!=1){
-        esc=1;
-        preencherLoja(listaFrutas);
+function escolha(botaoPressionado){
+    let botPress = Number(botaoPressionado.value);
+    if(esc!=botPress){
+        esc=botPress;
+        preencherLoja(listaEscolhas[botPress-1]);
+        //adiciona uma seleção visual ao botão e remove dos demais
+        escolheBotao(botPress);
+        //Remove o estado de fechado para a lista se expandir
+        listaHTML.classList.remove("fechado");
     }
     else{
-        listaHTML.innerHTML='';
-        listaAtiva=[];
         esc = 0;
-    }
-}
-
-//Laticineos
-function escolheLaticineos(){
-    if (esc!=2){
-        esc=2;
-        preencherLoja(listaLaticineos);
-    }
-    else{
-        listaHTML.innerHTML='';
-        listaAtiva=[];
-        esc = 0;
-    }
-}
-
-//Congelados
-function escolheCongelados(){
-    if (esc!=3){
-        esc=3;
-        preencherLoja(listaCongelados);
-    }
-    else{
-        listaHTML.innerHTML='';
-        listaAtiva=[];
-        esc = 0;
-    }
-}
-
-//Doces
-function escolheDoces(){
-    if (esc!=4){
-        esc=4;
-        preencherLoja(listaDoces);
-    }
-    else{
-        listaHTML.innerHTML='';
-        listaAtiva=[];
-        esc = 0;
-    }
-}
-
-//Outros
-function escolheOutros(){
-    if (esc!=5){
-        esc=5;
-        preencherLoja(listaOutros);
-    }
-    else{
-        listaHTML.innerHTML='';
-        listaAtiva=[];
-        esc = 0;
+        //Remove a seleção visual do botão
+        listaEscolhasHTML[botPress-1].classList.remove("ativo");
+        //Adiciona o estado de fechado para a lista se recolher
+        listaHTML.classList.add("fechado");
     }
 }
 
@@ -144,11 +103,11 @@ function preencherLoja(listaEsc){
     //Para cada item no array do tipo de item, cria um item de lista com:
     //Nome, preco, input de entrada da quantidade e botao adicionar
     for(let i =0; i<listaEsc.length;i++){
-        let nome = document.createElement('label');
+        let nome = document.createElement('p');
         nome.textContent = listaEsc[i][0];
         nome.classList.add("escopo__loja__lista__item__prod");
 
-        let preco = document.createElement('label');
+        let preco = document.createElement('p');
         //define que sera o preco em moeda Real R$0.00
         preco.textContent = "R$"+listaEsc[i][1].toFixed(2);
         preco.classList.add("escopo__loja__lista__item__preco");
@@ -156,7 +115,7 @@ function preencherLoja(listaEsc){
         let input = document.createElement('input');
         input.type = 'number';
         input.min = '0';
-        input.placeholder = 'Qtd';
+        input.placeholder = 'Qtd.';
         input.id = 'entrada_'+i;
         input.classList.add("escopo__loja__lista__item__input");
 
@@ -182,10 +141,24 @@ function preencherLoja(listaEsc){
     }
 }
 
+// Adiciona uma seleção visual do botão escolhido e remove a dos demais
+function escolheBotao(valorBotaoEscolhido){
+    for (i=0; i<listaEscolhasHTML.length; i++){
+        if (valorBotaoEscolhido-1==i){
+            listaEscolhasHTML[i].classList.add("ativo");
+        }
+        else{
+            listaEscolhasHTML[i].classList.remove("ativo");
+        }
+    }
+}
+
+//Carrinho
 //Funcao de adicao no carrinho de compras
 function adicionarNoCarrinho(){
+    let botao = this;
     //pega o valor do botao pressionado (definido anteriormente como o indice da lista)
-    let id = Number(this.value);
+    let id = Number(botao.value);
     //pega o valor do input da entrada da lista escolhida e retorna seu valor
     let input = 'entrada_'+id;
     let inputHTML = document.getElementById(input);
@@ -193,9 +166,12 @@ function adicionarNoCarrinho(){
     inputHTML.value = '';
 
     //verifica se o valor não é 0 ou negativo
-    if(qtd <= 0){
+    if(qtd <= 0 || isNaN(qtd)){
+        addFalse(botao);
         return;
     }
+
+    addTrue(botao);
 
     //transforma em inteiro a quantidade
     qtd = Number(qtd);
@@ -219,64 +195,103 @@ function adicionarNoCarrinho(){
 
     //adiciona a lista na lista do carrinho e atualiza a listaCarrinhoHTML
     listaCarrinho.push(lista);
-    atualizarCarrinhoHTML();
+    atualizarCarrinhoHTML(true);
+}
+
+function addTrue(botao){
+    botao.classList.add("true");
+    setTimeout(function(){
+        botao.classList.remove("true");
+    },100);
+}
+
+function addFalse(botao){
+    botao.classList.add("false");
+    setTimeout(function(){
+        botao.classList.remove("false");
+    },100);
 }
 
 //Funcao de atualizacao da lista do html do carrinho
-function atualizarCarrinhoHTML(){
-    listaCarrinhoHTML.innerHTML='';    
+//Se acaso for true, realizará uma animação na lista
+function atualizarCarrinhoHTML(util=false){
+    listaCarrinhoHTML.innerHTML='';   
+    let tam = listaCarrinho.length; 
     //para todo array existente na lista do carrinho, ele criara uma li com as seguintes
     //atribuicoes:
     //nome, preco, botaoDiminuir, qtd, botaoIncrementar e botaoExcluir da lista
     //Nos botoes, value = i para evento futuro
-    for(let i = 0; i< listaCarrinho.length;i++){
+    if (tam>0){
+        for(let i = 0; i< tam;i++){
 
-        let nome = document.createElement('label');
-        nome.textContent=listaCarrinho[i][0];
-        nome.classList.add("escopo__carrinho__lista__item__prod");
+            let nome = document.createElement('p');
+            nome.textContent=listaCarrinho[i][0];
+            nome.classList.add("escopo__carrinho__lista__item__prod");
 
-        let preco = document.createElement('label');
-        preco.textContent="R$"+listaCarrinho[i][1].toFixed(2);
-        preco.classList.add("escopo__carrinho__lista__item__preco");
+            let preco = document.createElement('p');
+            preco.textContent="R$"+listaCarrinho[i][1].toFixed(2);
+            preco.classList.add("escopo__carrinho__lista__item__preco");
 
-        let botaoRem = document.createElement('button');
-        botaoRem.textContent='-';
-        botaoRem.value=i;
-        //Adiciona o evento, ao clicar, diminuirValor()
-        botaoRem.addEventListener('click', diminuirValor);
-        botaoRem.classList.add("escopo__carrinho__lista__item__botao");
+            let botaoRem = document.createElement('button');
+            botaoRem.textContent='-';
+            botaoRem.value=i;
+            //Adiciona o evento, ao clicar, diminuirValor()
+            botaoRem.addEventListener('click', diminuirValor);
+            botaoRem.classList.add("escopo__carrinho__lista__item__casaQtd__botao");
 
-        let qtd = document.createElement('label');
-        qtd.textContent = listaCarrinho[i][2];
-        qtd.classList.add("escopo__carrinho__lista__item__qtd");
+            let qtd = document.createElement('p');
+            qtd.textContent = listaCarrinho[i][2];
+            qtd.classList.add("escopo__carrinho__lista__item__casaQtd__qtd");
 
-        let botaoAdd = document.createElement('button');
-        botaoAdd.textContent='+';
-        botaoAdd.value = i;
-        //Adiciona o evento, ao clicar, adicionarValor()
-        botaoAdd.addEventListener('click', adicionarValor);
-        botaoAdd.classList.add("escopo__carrinho__lista__item__botao");
+            let botaoAdd = document.createElement('button');
+            botaoAdd.textContent='+';
+            botaoAdd.value = i;
+            //Adiciona o evento, ao clicar, adicionarValor()
+            botaoAdd.addEventListener('click', adicionarValor);
+            botaoAdd.classList.add("escopo__carrinho__lista__item__casaQtd__botao");
 
-        let botaoExcluir = document.createElement('button');
-        botaoExcluir.textContent='X';
-        botaoExcluir.value=i;
-        //Adiciona o evento, ao clicar, excluirDoCarrinho()
-        botaoExcluir.addEventListener('click', excluirDoCarrinho);
-        botaoExcluir.classList.add("escopo__carrinho__lista__item__botaoExcluir");
+            let casaQtd = document.createElement('div');
+            casaQtd.classList.add("escopo__carrinho__lista__item__casaQtd")
+            casaQtd.appendChild(botaoRem);
+            casaQtd.appendChild(qtd);
+            casaQtd.appendChild(botaoAdd);
 
-        //cria a lista e define tudo dentro da mesma li
-        let item = document.createElement('li');
-        item.classList.add("escopo__carrinho__lista__item");
+            let botaoExcluir = document.createElement('button');
+            botaoExcluir.textContent='X';
+            botaoExcluir.value=i;
+            //Adiciona o evento, ao clicar, excluirDoCarrinho()
+            botaoExcluir.addEventListener('click', excluirDoCarrinho);
+            botaoExcluir.classList.add("escopo__carrinho__lista__item__botaoExcluir");
 
-        item.appendChild(nome);
-        item.appendChild(preco);
-        item.appendChild(botaoRem);
-        item.appendChild(qtd);
-        item.appendChild(botaoAdd);
-        item.appendChild(botaoExcluir);
+            
 
-        //adiciona a lista para a listaCarrinhoHTML (html da lista do carrinho)
-        listaCarrinhoHTML.appendChild(item);
+            //cria a lista e define tudo dentro da mesma li
+            let item = document.createElement('li');
+            item.classList.add("escopo__carrinho__lista__item");
+
+            item.appendChild(nome);
+            item.appendChild(preco);
+            item.appendChild(casaQtd);
+            item.appendChild(botaoExcluir);
+
+            if(util){
+                item.classList.add('novo');
+            }
+            //adiciona a lista para a listaCarrinhoHTML (html da lista do carrinho)
+            listaCarrinhoHTML.appendChild(item);
+
+            if(util){
+                setTimeout(() => {
+                    item.classList.remove("novo");
+                }, 50); 
+            }
+        }
+    }
+    else{
+        let nenhum = document.createElement('li');
+        nenhum.classList.add("escopo__carrinho__lista__nothing");
+        nenhum.textContent = "Nenhum";
+        listaCarrinhoHTML.appendChild(nenhum);
     }
     //atualiza o total
     atualizarTotal();
@@ -285,11 +300,21 @@ function atualizarCarrinhoHTML(){
 //Incrementa a quantidade do item do carrinho, desde que a quantidade nao seja
 //menor que 0, se for menor que 0, ele ira remover da lista
 function diminuirValor(){
-    let id = Number(this.value);
+    let botao = this;
+    let id = Number(botao.value);
     let qtdAtual = listaCarrinho[id][2];
     if(qtdAtual>0){
-        listaCarrinho[id][2]-=1;
-        atualizarCarrinhoHTML();
+        
+        // botao.classList.add('true');
+
+        // setTimeout(function(){
+        //     listaCarrinho[id][2]-=1;
+        //     botao.classList.remove('true');
+        //     atualizarCarrinhoHTML();
+        // },100);
+
+        listaCarrinho[id][2]-=1; 
+        atualizarCarrinhoHTML();       
     }
     else{
         excluirDoCarrinhoMin(id);
@@ -298,17 +323,27 @@ function diminuirValor(){
 
 //incrementa a quantidade do item no carrinho 
 function adicionarValor(){
-    let id = Number(this.value);
+    let botao = this;
+    let id = Number(botao.value);
+
+    // botao.classList.add('true');
+
+    // setTimeout(function(){
+    //     listaCarrinho[id][2]+=1;
+    //     botao.classList.remove('true');
+    //     atualizarCarrinhoHTML();
+    // },100);
+
     listaCarrinho[id][2]+=1;
     atualizarCarrinhoHTML();
 }
 
 //exclui o item do carrinho
 function excluirDoCarrinho(){
-    let id = Number(this.value);
-    
+    let botao = this;
+    let id = Number(botao.value);
     listaCarrinho.splice(id,1);
-    atualizarCarrinhoHTML();
+    atualizarCarrinhoHTML(true);
 }
 
 //exclui o item do carrinho se for diminuido direto da funcao 'diminuirValor()'
@@ -334,7 +369,9 @@ function atualizarTotal(){
 function reiniciar(){
     listaCarrinho = [];
     atualizarCarrinhoHTML();
-    listaHTML.innerHTML='';
-    listaAtiva=[];
+    listaHTML.classList.add("fechado");
     esc = 0;
+    for (i=0; i<listaEscolhasHTML.length; i++){
+        listaEscolhasHTML[i].classList.remove('ativo');
+    }
 }
