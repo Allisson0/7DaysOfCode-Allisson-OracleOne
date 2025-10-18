8f7485f2622df21f4cdc02977b9442ca41a695b3
Allisson0 <allissonhiaggo@gmail.com>
2025-10-17 21:57:28 -0300

    Refatoração de apagar e documentação do código

--- Pasta de Desafios/DaySeven/codigoDaySeven.js
+++ Pasta de Desafios/DaySeven/codigoDaySeven.js
@@ -1,16 +1,27 @@
+//Declaração de constantes - Tela e texto tela ocioso
 const SCREEN = document.getElementById("telaCalculadora");
 const initScreen = "Calc. 3.000";
 
+//Variavel de verificação se o último digito foi 
+// uma expressão ou se existe na secção o ponto
 let hasDouble = false;
 let hasAction = false;
 
+//Listas de ações - números e expressões
 let number = [''];
 let action = [''];
+
+//Texto para tela
 let textNumberAction = "";
+
+//Secção atual
 let section = 0;
 
+//Função de digitar número - recebe o botão clicado para verificar valor
 function digitar(button){
 
+    //Se há uma ação no momento da digitação
+    //cria uma nova secção
     if(hasAction){
         section++;
         number.push('');
@@ -18,77 +29,90 @@ function digitar(button){
         hasDouble= false;
     }
 
+    //ação não é o último digito digitado
     hasAction = false;
 
+    //Botão escolhido
     let choose = button.value;
 
-    if (choose == '0' && number[section] == "0"){
+    //Verifica se a escolha é 0 e se for 0 e já houver um zero em secção, impede
+    //a ação
+    if (choose == '0' && number[section] == "0") {
         return;
     }
 
+    //Adiciona o número escolhido na secção do número
     number[section] += choose;
-    updateScreen();
 
+    //Função de atualização da tela
+    updateScreen();
 }
 
+//Digita uma expressão dada pelo botão
 function acao(button){
 
-    if (!hasAction && number[section].length > 0) {
+    //Se houver pelo menos um número na lista de número da secção atual,
+    //insere a expressão
+    if (number[section].length > 0) {
         hasAction = true;
-
-        let lastThing = number[section][-1];
-
-        if (lastThing != button.value) {
-            action[section] = button.value;
-        }        
+        action[section] = button.value;      
         updateScreen();
     } 
-    else if (number[section].length > 0) {
-        action[section] = button.value;
-        updateScreen();
-    }
-    
 }
 
+//Função de pontuar número
 function pontuar(button){
-
     let point = button.value;
 
+    //Se não houver pontuação na secção, insere o ponto
     if (!hasDouble){
+
+        //Se for diferente de nada, insere o ponto
         if (number[section] != ''){
             number[section]+=point;
         }
+
+        //Caso seja nada, insere um 0 e o ponto
         else{
             number[section]+='0';
             number[section]+=point;
         }
+        
+        //Define a existência de pontuação
         hasDouble = true;
         updateScreen();
     }
-    
 }
 
-
+//Função de apagar
 function apagar(){
 
-    if (!hasAction){
+    //Se não houver expressão, apaga dos números se houver número
+    if (!hasAction) {
         if (number[section].length>0) {
             number[section] = number[section].slice(0, -1);
             updateScreen();
         }
-    } else{
+    } 
+    //caso seja uma expressão, deleta de expressão e define que o último
+    //digito é um número
+    else{
         action[section] = action[section].slice(0, -1);
         hasAction = false;
         updateScreen();
     }
 
-    if (number[section][number[section].length -1] == '.') {
+    //Se houver ponto na secção, define que atualmente há pontuação
+    if (number[section].includes('.')) {
         hasDouble=true;
-    } else{
+    } //caso não, não há
+    else{
         hasDouble = false;
     }
 
-    if (number[section] == 0 && section != 0){
+    //Se for o último digito da secção, e a secção não for 0,
+    //diminui a secção e os arrays
+    if (number[section].length == 0 && section != 0) {
         number.pop();
         action.pop();
         section--;
@@ -96,12 +120,14 @@ function apagar(){
         hasAction=true;        
     }    
 
-    if (number[0] == ''){
+    //Se não houver mais valores, volta a tela inicial
+    if (number[0] == '') {
             SCREEN.innerHTML = initScreen;
             return;
         }
 }
 
+//Retorna os valores a zero
 function limpar(){
     number = [''];
     action = [''];
@@ -112,16 +138,19 @@ function limpar(){
     SCREEN.innerHTML = initScreen;
 }
 
-
+//Função de retorno do resultado
 function verificar(){
     let result = 0;
-
-    console.log(number);
-    console.log(action);
-
+    
+    //Se a última secção de action for '', e o tamanho do array números
+    //for pelo menos 2, ele irá fazer a conta
     if (action[action.length-1] == '' && number.length>1) {
+
+        //Para cada item na secção de ação, realiza uma expressão até o
+        //o penúltimo valor, pois o último é ''
         for (let i = 0; i < action.length-1; i++) {
 
+            //Caso tal, resultado é o resultado da expressão devida
             switch (action[i]) {
                 case '+':
                     result = parseFloat(number[0]) + parseFloat(number[1]);
@@ -140,33 +169,42 @@ function verificar(){
                     break;
             }
 
+            //Remove o segundo item de número e define o primeiro sendo o resultado
             number.splice(1,1);
             number[0] = result;
-            
+
+            //Repete até o fim das operações
         }
         
+        //Define que não há mais expressões como último dígito
         hasAction = false;
 
+        //Se o resultado for real, define que há pontuação
         if (typeof result === 'number' && !Number.isInteger(result)) {
             hasDouble=true;
         }else{
             hasDouble=false;
         }
 
+        //numero começa com o resultado e expressão não contém nada
         number = [''+result];
         action = [''];
+        //Retorna pra a secção inicial
         section=0;
-        console.log(number);
-        console.log(action);
         updateScreen();
     }    
 }
 
+//Atualiza a tela
 function updateScreen(){
     textNumberAction = '';
+    
+    //Para cada item em number, adiciona e posteriormente adiciona o item de action
+    //de mesmo índice
     for (let i = 0; i < number.length; i++){
         textNumberAction += number[i];
         textNumberAction += action[i];
     }
+    //Imprime na tela SCREEN
     SCREEN.innerHTML=textNumberAction;
 }
